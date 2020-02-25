58f719595dfb472d01046c2101a50503c5dcc8ba
unknown <tranthuthao87@gmail.com>
2020-02-25 14:59:03 +0700

    Alert popup handling

---
+++ ComponentHelper/JavaScriptPopupHelper.cs
@@ -0,0 +1,39 @@
+﻿using OpenQA.Selenium;
+using SeleniumWebdriver.Settings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumWebdriver.ComponentHelper
+{
+    public class JavaScriptPopupHelper
+    {
+        public static bool IsPopupPresent()
+        {
+            try
+            {
+                ObjectRepository.Driver.SwitchTo().Alert();
+                return true;
+            }
+            catch (NoAlertPresentException)
+            {
+                return false;
+            }
+        }
+        public static string GetPopupText()
+        {
+            if (!IsPopupPresent())
+                return string.Empty;
+            return ObjectRepository.Driver.SwitchTo().Alert().Text;
+        }
+        public static void ClickOKonPopup()
+        {
+            if (!IsPopupPresent())
+                return;
+            ObjectRepository.Driver.SwitchTo().Alert().Accept();
+        }
+        
+    }
+}
---
+++ Testscript/Popup/TestPopup.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using SeleniumWebdriver.ComponentHelper;
+using SeleniumWebdriver.Settings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SeleniumWebdriver.Testscript.Popup
+{
+    [TestClass]
+    public class TestPopup
+    {
+        [TestMethod]
+        public void TestAlert()
+        {
+            //NavigationHelper.NavigationToURL("https://www.w3schools.com/js/js_popup.asp");
+            //ButtonHelper.ClickOnButton(By.XPath("//div[@id='main']/descendant::a[position()=3]"));
+            //BrowserHelper.SwitchToWindow(1);
+            //switch to iframe to work on Iframe
+            NavigationHelper.NavigationToURL("https://www.w3schools.com/js/tryit.asp?filename=tryjs_alert");
+            ObjectRepository.Driver.SwitchTo().Frame(ObjectRepository.Driver.FindElement(By.Id("iframeResult")));
+            ButtonHelper.ClickOnButton(By.XPath("//button[text()='Try it']"));
+            if(JavaScriptPopupHelper.IsPopupPresent())
+            {
+                var text = JavaScriptPopupHelper.GetPopupText();
+                JavaScriptPopupHelper.ClickOKonPopup();
+                ObjectRepository.Driver.SwitchTo().DefaultContent();
+                // Add this line before the line of code , where you are trying to clear the text area
+                IWebElement textarea = ObjectRepository.Driver.FindElement(By.Id("textareaCode"));
+                ExecuteScript("document.getElementById('textareaCode').setAttribute('style','display: inline;')");
+
+                TextBoxHelper.ClearTextbox(By.Id("textareaCode"));
+                TextBoxHelper.TypeInTextbox(By.Id("textareaCode"), text);
+            }       
+        }
+        public static object ExecuteScript(string script)
+        {
+            IJavaScriptExecutor executor = ((IJavaScriptExecutor)ObjectRepository.Driver);
+
+            return executor.ExecuteScript(script);
+
+        }
+        
+    }
+}
