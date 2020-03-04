6f0d8a3cf0ed47153d518a374afdc83fac511e2c
unknown <tranthuthao87@gmail.com>
2020-03-04 17:50:05 +0700

    Read data from csv file

--- Testscript/DataDriven/TestCreateBug.cs
+++ Testscript/DataDriven/TestCreateBug.cs
@@ -13,15 +13,23 @@ namespace SeleniumWebdriver.Testscript.DataDriven
     [TestClass]
     public class TestCreateBug
     {
+        private TestContext _testContext;
+        public TestContext TestContext
+        {
+            get { return _testContext; }
+            set { _testContext = value; }
+        }
+        
         [TestMethod]
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV", @"D:\Git\SeleniumWebdriver\DataFiles\CreateData.csv", "CreateData#csv", DataAccessMethod.Sequential)]
         public void CreateABug()
         {
             NavigationHelper.NavigationToURL(ObjectRepository.Config.GetWebsite());
             HomePage hPage = new HomePage(ObjectRepository.Driver);
             LoginPage loginPage = hPage.NavigateToLogin();
             BugDetail bugDetail = loginPage.LoginToPage(ObjectRepository.Config.GetUsername(), ObjectRepository.Config.GetPassword());
-            bugDetail.SelectDropdownList("minor", "Other", "Linux");
-            bugDetail.FillInTextbox("summary", "description");
+            bugDetail.SelectDropdownList(TestContext.DataRow["Severity"].ToString(), TestContext.DataRow["HardWare"].ToString(), TestContext.DataRow["OS"].ToString());
+            bugDetail.FillInTextbox(TestContext.DataRow["Summary"].ToString(), TestContext.DataRow["Desc"].ToString());
             bugDetail.SubmitBug();
             hPage = bugDetail.Logout();
         }
---
+++ Testscript/TestClassContext/TestClassContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumWebdriver.Testscript.TestClassContext
+{
+    [TestClass]
+    public class TestClassContext
+    {
+        private TestContext _testContext;
+        public TestContext TestContext
+        {
+            get
+            { return _testContext; }
+            set
+            { _testContext = value; }
+        }
+        [TestMethod]
+        public void Testcase1()
+        {
+            Console.WriteLine($"Test name: {TestContext.TestName}");
+        }
+        [TestMethod]
+        public void Testcase2()
+        {
+            Console.WriteLine($"Test name: {TestContext.TestName}");
+        }
+
+    }
+}
